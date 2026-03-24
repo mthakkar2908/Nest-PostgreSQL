6f89d102eaa36fc2eea6ceb45cd53332746570f1
Malav Thakkar <mthakkar@sigmasolve.net>
2026-03-24 19:20:56 +0530

    Globally set validation pipe

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
-
   await app.listen(process.env.PORT ?? 4500);
 }
 void bootstrap();
